4d689618f7c008258132c8f8bf799c833007b258
Milesgui23 <102737331+Milesgui23@users.noreply.github.com>
2022-05-05 10:50:40 -0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,28 @@
+function nome (a){
+    return console.log(a)
+    }
+    
+    nome('miles');
+    
+
+function soma (a,b){
+return console.log(a + b)
+}
+
+soma (5,3);
+
+//function : cria uma função;
+//toda função tem um nome;
+//parâmetros
+
+function mult (a,b){
+    return console.log(a * b)
+    }
+    
+    mult (5,3);
+
+    function media (a,b,c,d){
+        let res = (a+b+c+d)/4
+        return console.log(res)
+    }
+    media (9,8,4,3);
